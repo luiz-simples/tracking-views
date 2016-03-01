dc97c74be478835e0af1a5e95157e1e56d4d8efc
Luiz Amorim <luiz.simples@gmail.com>
2016-03-01 18:03:33 -0300

    Tests to Form Contact

--- test/controllers/contact_controller_test.rb
+++ test/controllers/contact_controller_test.rb
@@ -9,10 +9,26 @@ class ContactControllerTest < ActionController::TestCase
   test "should view main header" do
     get :index
     assert_select "h1", "Contact#Page"
+    assert_select "form[action=\"/contact\"]", 1
   end
 
   test "should have form with field name" do
     get :index
     assert_select "form>input[name=\"name\"]", 1
+  end
+
+  test "should have form with field email" do
+    get :index
+    assert_select "form>input[name=\"email\"]", 1
+  end
+
+  test "should have form with field message" do
+    get :index
+    assert_select "form>textarea[name=\"message\"]", 1
+  end
+
+  test "should have form with send button" do
+    get :index
+    assert_select "form>input[type=\"submit\"]", 1
   end
 end
