b18f5ad4e48fbdf807cc487fb453bb9bf9ff5bf7
Luiz Amorim <luiz.simples@gmail.com>
2016-03-01 16:42:27 -0300

    #ContactPage - reject contact when without email

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -4,10 +4,24 @@ class ContactController < ApplicationController
   end
 
   def new
+    contact = Contact.new contact_params
+
+    if contact.invalid?
+      erros = contact.errors.full_messages.join(', ')
+      flash[:alert] = erros
+      return index
+    end
+
     redirect_to :success
   end
 
   def success
     render 'contact/success'
+  end
+
+  private
+
+  def contact_params
+    params.permit(:name, :email, :message)
   end
 end
--- test/integration/lead_flows_test.rb
+++ test/integration/lead_flows_test.rb
@@ -1,11 +1,21 @@
 require 'test_helper'
 
 class LoadFlowsTest < ActionDispatch::IntegrationTest
-  test "visitant should send contact" do
+  def send_contact_flow(contact)
     get "/contact"
     assert_response :success
 
-    post_via_redirect "/contact", name: 'Someone Name', email: 'someone@email.com', message: 'Someone Text'
+    post_via_redirect "/contact", contact
+  end
+
+  test "visitant should send contact" do
+    send_contact_flow name: 'Someone Name', email: 'someone@email.com', message: 'Someone Text'
     assert_equal '/success', path
+  end
+
+  test "reject contact when without email" do
+    send_contact_flow name: 'Someone Name', email: '', message: 'Someone Text'
+    assert_equal '/contact', path
+    assert_select "h2.error", "Email can't be blank, Email is not an email"
   end
 end
